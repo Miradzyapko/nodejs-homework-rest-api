ca8adfd166c6e1446e0c8fda6ab88034c0c0363d
Miradzyapko <mira.dzyapko@gmail.com>
2023-10-17 17:24:00 +0300

    add files

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,95 +1,25 @@
 const express = require("express");
-const HttpError = require("../../helpers/Errors");
-const Joi = require('joi')
+
+const controllers = require("../../controllers/controllers");
 const contactsRouter = express.Router()
-const contactService = require('../../models/contacts/index');
-
-
-const contactAddSchema = Joi.object({
-  name: Joi.string().required().messages({"any.required":`"name" missing required field`}),
-  email: Joi.string().email(),
-  phone: Joi.string().required()
-});
-
-
-contactsRouter.get('/', async(req, res, next) => {
-  try {
-  
- const result = await contactService.listContacts();
- res.json(result);
-  }
- catch(error) {
-  next(error); 
-  
-}
-})
 
 
 
 
-contactsRouter.get('/:contactId', async (req, res, next) => {
-  try {
-    const { contactId } = req.params;
-    const result = await contactService.getContactById(contactId);
-    if(!result) {
-      throw HttpError(404, `Contact with '${contactId}' is not found!`);
-    } 
+contactsRouter.get('/', controllers.getAll);
 
-    res.json(result);
-  } catch (error) {
-    next(error);
-  }
-});
 
-contactsRouter.post('/', async (req, res, next) => {
-  try {
-  
-    const { error } = contactAddSchema.validate(req.body);
-    if (error) {
-      throw HttpError(400, error.message);
-    }
-    const result = await contactService.addContact(req.body);
-    res.status(200).json(result);
-  } catch (error) {
-    next(error);
-  }
-});
+
+contactsRouter.get('/:contactId', controllers.getContactById);
+ 
+
+contactsRouter.post('/', controllers.addContact);
+
   
 
-contactsRouter.delete('/:contactId', async (req, res, next) => {
-  try {
-  const { contactId } = req.params;
-  const result = await contactService.removeContact(contactId);
-  if (!result) {
-    throw HttpError(404, `Contact with '${contactId}' is not found!`);
-  }
-  res.json({
-  message:"Contact deleted!"
-  })
-    }
-    catch(error) {
-      next (error);
-    }
-})
-contactsRouter.put('/:contactId', async (req, res, next) => {
-  try {
-    if(!Object.keys(req.body).length)
-    {
-      throw HttpError(400, `Missing fields`)
-    }
-    const { error } = contactAddSchema.validate(req.body);
-    if (error) {
-      throw HttpError(400, error.message);
-    }
-    const { contactId } = req.params;
-    const result = await contactService.updateContact(contactId, req.body);
-    if (!result) {
-      throw HttpError(404, `Contact with '${contactId}' is not found!`);
-    }
-    res.json(result);
-  } catch (error) {
-    next(error);
-  }
-});
+contactsRouter.delete('/:contactId', controllers.deleteContact);
+
+contactsRouter.put('/:contactId', controllers.updateContact);
+ 
 
 module.exports = contactsRouter;
--- server.js
+++ server.js
@@ -1,15 +1,18 @@
-const mongoose = require('mongoose')
-const app = require('./app');
+const mongoose = require("mongoose");
+const app = require("./app");
 
 /* Md3oY0uiHBnm32gQ */
-const DB_HOST ="mongodb+srv://Mira:Md3oY0uiHBnm32gQ@cluster0.gkw5pgd.mongodb.net/db-contacts?retryWrites=true&w=majority"
-mongoose.connect(DB_HOST)
+const { DB_HOST, PORT = 3000 } = process.env;
+
+
+mongoose
+.connect(DB_HOST)
 .then(()=> {
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+app.listen(PORT, () => {
+  console.log("Database connection successful")
+});
 })
 .catch(error => {
   console.log(error.message);
   process.exit(1);
-})
+});
